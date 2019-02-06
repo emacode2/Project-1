576825745727795f111d18ea8933a9e532098091
Emmanuel Asu <mannyasu2@gmail.com>
2019-02-06 14:23:44 -0500

    added event listener to choices

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello World");
+const choices1 = document.querySelectorAll(".cells");
+
+for (let i = 0; i < choices1.length; i++) {
+  choices1[i].addEventListener("click", function(evt) {
+    evt.target.style.backgroundColor = "red";
+  });
+}
+
+//document.getElementsByClassName("cells").innerHTML = "France";
+//let answers = cells.length
+//choices1[5].addEventListener("click", function (evt) {
+//       evt.target.style.backgroundColor = "green"
